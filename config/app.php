37dd91d4290dead755fb291b2aad19647503ff9f
David Gegelija <code@imdavid.xyz>
2016-11-19 09:35:22 +0200

    Modules service provider

--- config/app.php
+++ config/app.php
@@ -162,7 +162,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        App\Modules\ModulesServiceProvider::class
     ],
 
     /*
